c8f0fcdd285858b2638692f47d8309582a89881b
Danil Lomaev <d.lomaev@vk.team>
2022-05-28 16:28:26 +0300

    Initial Commit

---
+++ Kommunalka/KommunalkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KommunalkaApp.swift
+//  Kommunalka
+//
+//  Created by Данил Ломаев on 28.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct KommunalkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
